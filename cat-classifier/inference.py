ecaef53a26f16fc6610f5be166c06d807904d9be
Nivahsim <mishavinogradskii@gmail.com>
2025-06-18 09:35:16 +0300

    minor updates

--- cat-classifier/inference.py
+++ cat-classifier/inference.py
@@ -8,7 +8,8 @@ model = "./weights/cat_classifier_best.pt"
 cuda_device = "0" if torch.cuda.is_available() else "cpu"
 
 yolo_command = (
-    f'yolo task=detect mode=predict model={model} source="./dataset/test/Ghera_1.jpg"'
+    # f'yolo task=detect mode=predict model={model} source="./dataset/test/Ghera_1.jpg"'
+    f'yolo task=detect mode=predict model={model} source="/mnt/c/Users/user/Desktop/IMG_4765.mp4"'
 )
 
 # command = f"{yolo_command} batch=8 workers=2 device={cuda_device}"
